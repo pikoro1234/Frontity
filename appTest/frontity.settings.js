4c38c6c6d265c0e91dc3f9636c7e6fe695d30e10
Jorge <donjuan.jfc@gmail.com>
2021-02-24 23:21:52 +0100

    prueba cambio de titulo

--- appTest/frontity.settings.js
+++ appTest/frontity.settings.js
@@ -3,7 +3,7 @@ const settings = {
   "state": {
     "frontity": {
       "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
+      "title": "Mi primer Blog W&R",
       "description": "WordPress installation for Frontity development"
     }
   },
